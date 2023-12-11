da6578babc050474d83cd00fd6a58a233e0fccbd
Deyan Koba <118819632+deyan-koba-zupit@users.noreply.github.com>
2023-12-11 23:48:31 +0100

    add custom icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Icons } from '../assets/font-icons/icons';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-custom-icons-test';
+
+  readonly iconsEnum = Icons;
+
+  readonly docker = Icons.Docker;
+  readonly iconHtml = Icons.ProgrammingLanguagesHtml;
 }
--- src/assets/font-icons/icons.ts
+++ src/assets/font-icons/icons.ts
@@ -1,37 +1,37 @@
 export type IconsId =
-  | "c-sharp"
   | "docker"
   | "graphql"
-  | "html"
-  | "javascript"
   | "kubernetes"
+  | "programming-languages-c-sharp"
+  | "programming-languages-html"
+  | "programming-languages-javascript"
   | "typescript";
 
 export type IconsKey =
-  | "CSharp"
   | "Docker"
   | "Graphql"
-  | "Html"
-  | "Javascript"
   | "Kubernetes"
+  | "ProgrammingLanguagesCSharp"
+  | "ProgrammingLanguagesHtml"
+  | "ProgrammingLanguagesJavascript"
   | "Typescript";
 
 export enum Icons {
-  CSharp = "c-sharp",
   Docker = "docker",
   Graphql = "graphql",
-  Html = "html",
-  Javascript = "javascript",
   Kubernetes = "kubernetes",
+  ProgrammingLanguagesCSharp = "programming-languages-c-sharp",
+  ProgrammingLanguagesHtml = "programming-languages-html",
+  ProgrammingLanguagesJavascript = "programming-languages-javascript",
   Typescript = "typescript",
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
-  [Icons.CSharp]: "61697",
-  [Icons.Docker]: "61698",
-  [Icons.Graphql]: "61699",
-  [Icons.Html]: "61700",
-  [Icons.Javascript]: "61701",
-  [Icons.Kubernetes]: "61702",
+  [Icons.Docker]: "61697",
+  [Icons.Graphql]: "61698",
+  [Icons.Kubernetes]: "61699",
+  [Icons.ProgrammingLanguagesCSharp]: "61700",
+  [Icons.ProgrammingLanguagesHtml]: "61701",
+  [Icons.ProgrammingLanguagesJavascript]: "61702",
   [Icons.Typescript]: "61703",
 };
